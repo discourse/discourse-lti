804d495b8137f8abc2ee925c73873e996203bc79
David Taylor <david@taylorhq.com>
2021-05-18 19:03:45 +0100

    DEV: Correct eslint failures

--- assets/javascripts/discourse-lti/api-initializers/hide-lti-login-button.js
+++ assets/javascripts/discourse-lti/api-initializers/hide-lti-login-button.js
@@ -1,12 +1,11 @@
 import { apiInitializer } from "discourse/lib/api";
-import { findAll } from "discourse/models/login-method";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default apiInitializer("0.8", (api) => {
   api.modifyClass("component:login-buttons", {
     @discourseComputed
-    buttons(){
-      return this._super().filter((m) => m.name != "lti")
-    }
+    buttons() {
+      return this._super().filter((m) => m.name !== "lti");
+    },
   });
 });
